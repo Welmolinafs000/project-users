d677887061fe1aa4447f5ee1ee870d70dcd6e44f
Welmolinafs000 <welmolinafs000@gmail.com>
2024-05-02 23:46:44 -0300

    project-users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Container, Image, ContainerItens, Title, InputLabel, Input, Button, User } from './styles';
+import People from './assets/peopple.svg';
+import Arrow from './assets/arrow.svg'
+import Trash from './assets/trash.svg'
 
-function App() {
+const App = () => {
+  const [users, setUsers] = useState([])
+  const [name, setName] = useState()
+  const [age, setAge] = useState()
+
+  const addNewUser = () => {
+    setUsers([...users, {id: Math.random(), name, age}])
+  }
+
+  const changeImputname = (event) => {
+    setName(event.target.value)
+  }
+
+  const changeImputAge = (event) => {
+    setAge(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Image src={People} alt="people" />
+      <ContainerItens>
+        <Title> Hello !!!</Title>
+
+        <Input onChange={changeImputname} placeholder="Nome" />
+
+        <InputLabel>Idade</InputLabel>
+        <Input onChange={changeImputAge} placeholder="Idade" />
+
+        <Button onClick={addNewUser}>Cadastrar
+          <img src={Arrow} alt="seta" />
+        </Button>
+
+        <ul>
+          {users.map((user) => (
+            <User key={user.id}>
+              <p>{user.name}</p><p>{user.age}</p>
+              <button> <img src={Trash} alt="lata-de-lixo"/> </button>
+            </User>
+          ))}
+        </ul>
+      </ContainerItens>
+
+    </Container>
+  )
+
 }
 
 export default App;
